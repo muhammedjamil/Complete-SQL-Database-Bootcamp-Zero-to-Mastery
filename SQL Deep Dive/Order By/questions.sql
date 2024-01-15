10899d215306fc15aa52739489e26d772bccf40e
muhammedjamil <104040790+muhammedjamil@users.noreply.github.com>
2024-01-15 02:59:33 +0200

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -10,10 +10,11 @@
 * Table: employees
 * Question: Sort employees by age
 */
-
+SELECT "public"."employees"."first_name", "public"."employees"."last_name", (EXTRACT('year'from age( "public"."employees"."birth_date"))) AS "Age" FROM employees order BY  "Age" deSC
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+SELECT "public"."employees"."first_name", "public"."employees"."last_name","public"."employees"."hire_date" FROM employees WHERE "public"."employees"."first_name" ILIKE 'k%' ORDER BY "public"."employees"."hire_date" 
